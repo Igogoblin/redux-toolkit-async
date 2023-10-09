53483a678e169149837722633d6a332b34e08794
Ihar Skavysh <igogoblin@gmail.com>
2023-10-09 21:22:19 +0300

    init: repeat base layout

---
+++ redux-toolkit-async/src/components/TodoItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleComplete, removeTodo } from "../store/todoSlice";
+import "../App";
+
+function TodoItem({ id, text, completed }) {
+  const dispatch = useDispatch();
+  return (
+    <li>
+      <input
+        className="check"
+        type="checkbox"
+        checked={completed}
+        onChange={() => dispatch(toggleComplete({ id }))}
+      />
+      <span>{text}</span>
+      <span className="dell" onClick={() => dispatch(removeTodo({ id }))}>
+        &times;
+      </span>
+    </li>
+  );
+}
+
+export default TodoItem;
